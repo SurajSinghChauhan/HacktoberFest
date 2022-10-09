c833eab91393deed5ffcaee44d1e342053f99a35
RaudraWarrior007 <45724841+RaudraWarrior007@users.noreply.github.com>
2022-10-09 20:11:49 +0530

    Update palindrome Example 2022 (#47)

--- PalinExample2.java
+++ PalinExample2.java
@@ -11,8 +11,8 @@ class PalindromeExample2
       for ( int i = length - 1; i >= 0; i-- )  
          reverse = reverse + original.charAt(i);  
       if (original.equals(reverse))  
-         System.out.println("Entered string/number is a palindrome.");  
+         System.out.println("String/number is a palindrome.");  
       else  
-         System.out.println("Entered string/number isn't a palindrome.");   
+         System.out.println("String/number isn't a palindrome.");   
    }  
 }  
